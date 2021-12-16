57155e433e5fdf75db24f08043536f91c60e18ec
louiemc <clouie2@gmail.com>
2021-12-15 22:13:19 -0500

    connected project1 to index.html, added dom elements to project1 html, defined consts in project1 js and color array - on button click a random color is generated and the random color is displayed in the console

---
+++ app/projects/1-change-background-color/project1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Grab elements to manipulate DOM
+const colorName = document.getElementById("color-name");
+const changeColorBtn = document.getElementById("change-color-btn");
+
+// Array of colors
+const colorArr = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+// On button click generate a random color
+changeColorBtn.addEventListener("click", function () {
+  let randomColorNumber = Math.floor(Math.random() * colorArr.length);
+  console.log(randomColorNumber);
+  let randomColor = colorArr[randomColorNumber];
+  console.log(randomColor);
+});
